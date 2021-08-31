f194d5e901cd9f03a9e701c9d06ee45eb1f7e410
knw7x9 <katherinewilsdon@gmail.com>
2021-08-31 17:46:19 -0600

    Pipeline should fail

--- yapf.sh
+++ yapf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-STATUS=`yapf --in-place --recursive . --style={column_limit:120}`
+STATUS=`yapf --diff --recursive . --style={column_limit:120}`
 if [[ ! -z $STATUS ]]; then
     echo "Run 'yapf --in-place --recursive . --style={column_limit:120}' command in the root of the repository."
     exit 1
