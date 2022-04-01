06ebc63926fe59d9d7f1abbc5cb7741ae3605a72
CICD <noreply@cicd.com>
2022-04-01 15:47:51 +0000

    9bd1d0dfd1addda40a9ab60fc3cfc96c3a24f315

--- deep_lynx/api/data_query_api.py
+++ deep_lynx/api/data_query_api.py
@@ -32,10 +32,224 @@ class DataQueryApi(object):
             api_client = ApiClient()
         self.api_client = api_client
 
-    def query_graph(self, body, container_id, **kwargs):  # noqa: E501
-        """Query Graph  # noqa: E501
+    def query_data(self, body, container_id, **kwargs):  # noqa: E501
+        """Query Data  # noqa: E501
 
-        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  # noqa: E501
+        Query data from your container using GraphQL. You can learn more here - https://gitlab.software.inl.gov/b650/Deep-Lynx/-/wikis/Querying-Data-With-GraphQL  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.query_data(body, container_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param object body: (required)
+        :param str container_id: (required)
+        :return: InlineResponse200
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.query_data_with_http_info(body, container_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.query_data_with_http_info(body, container_id, **kwargs)  # noqa: E501
+            return data
+
+    def query_data_with_http_info(self, body, container_id, **kwargs):  # noqa: E501
+        """Query Data  # noqa: E501
+
+        Query data from your container using GraphQL. You can learn more here - https://gitlab.software.inl.gov/b650/Deep-Lynx/-/wikis/Querying-Data-With-GraphQL  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.query_data_with_http_info(body, container_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param object body: (required)
+        :param str container_id: (required)
+        :return: InlineResponse200
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['body', 'container_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method query_data" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'body' is set
+        if ('body' not in params or
+                params['body'] is None):
+            raise ValueError("Missing the required parameter `body` when calling `query_data`")  # noqa: E501
+        # verify the required parameter 'container_id' is set
+        if ('container_id' not in params or
+                params['container_id'] is None):
+            raise ValueError("Missing the required parameter `container_id` when calling `query_data`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'container_id' in params:
+            path_params['container_id'] = params['container_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if 'body' in params:
+            body_params = params['body']
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json', 'text/plain'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['BearerAuth']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/containers/{container_id}/data', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='InlineResponse200',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def query_data(self, body, container_id, **kwargs):  # noqa: E501
+        """Query Data  # noqa: E501
+
+        Query data from your container using GraphQL. You can learn more here - https://gitlab.software.inl.gov/b650/Deep-Lynx/-/wikis/Querying-Data-With-GraphQL  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.query_data(body, container_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param object body: (required)
+        :param str container_id: (required)
+        :return: InlineResponse200
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        if kwargs.get('async_req'):
+            return self.query_data_with_http_info(body, container_id, **kwargs)  # noqa: E501
+        else:
+            (data) = self.query_data_with_http_info(body, container_id, **kwargs)  # noqa: E501
+            return data
+
+    def query_data_with_http_info(self, body, container_id, **kwargs):  # noqa: E501
+        """Query Data  # noqa: E501
+
+        Query data from your container using GraphQL. You can learn more here - https://gitlab.software.inl.gov/b650/Deep-Lynx/-/wikis/Querying-Data-With-GraphQL  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.query_data_with_http_info(body, container_id, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool
+        :param object body: (required)
+        :param str container_id: (required)
+        :return: InlineResponse200
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        all_params = ['body', 'container_id']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        params = locals()
+        for key, val in six.iteritems(params['kwargs']):
+            if key not in all_params:
+                raise TypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method query_data" % key
+                )
+            params[key] = val
+        del params['kwargs']
+        # verify the required parameter 'body' is set
+        if ('body' not in params or
+                params['body'] is None):
+            raise ValueError("Missing the required parameter `body` when calling `query_data`")  # noqa: E501
+        # verify the required parameter 'container_id' is set
+        if ('container_id' not in params or
+                params['container_id'] is None):
+            raise ValueError("Missing the required parameter `container_id` when calling `query_data`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'container_id' in params:
+            path_params['container_id'] = params['container_id']  # noqa: E501
+
+        query_params = []
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        if 'body' in params:
+            body_params = params['body']
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['application/json'])  # noqa: E501
+
+        # HTTP header `Content-Type`
+        header_params['Content-Type'] = self.api_client.select_header_content_type(  # noqa: E501
+            ['application/json', 'text/plain'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['BearerAuth']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/containers/{container_id}/data', 'POST',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='InlineResponse200',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=params.get('async_req'),
+            _return_http_data_only=params.get('_return_http_data_only'),
+            _preload_content=params.get('_preload_content', True),
+            _request_timeout=params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
+    def query_graph(self, body, container_id, **kwargs):  # noqa: E501
+        """Query Graph (Deprecated)  # noqa: E501
+
+        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  This has been deprecated in favor of the `/containers/{container_id}/data` endpoint.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.query_graph(body, container_id, async_req=True)
@@ -56,9 +270,9 @@ class DataQueryApi(object):
             return data
 
     def query_graph_with_http_info(self, body, container_id, **kwargs):  # noqa: E501
-        """Query Graph  # noqa: E501
+        """Query Graph (Deprecated)  # noqa: E501
 
-        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  # noqa: E501
+        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  This has been deprecated in favor of the `/containers/{container_id}/data` endpoint.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.query_graph_with_http_info(body, container_id, async_req=True)
@@ -140,9 +354,9 @@ class DataQueryApi(object):
             collection_formats=collection_formats)
 
     def query_graph(self, body, container_id, **kwargs):  # noqa: E501
-        """Query Graph  # noqa: E501
+        """Query Graph (Deprecated)  # noqa: E501
 
-        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  # noqa: E501
+        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  This has been deprecated in favor of the `/containers/{container_id}/data` endpoint.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.query_graph(body, container_id, async_req=True)
@@ -163,9 +377,9 @@ class DataQueryApi(object):
             return data
 
     def query_graph_with_http_info(self, body, container_id, **kwargs):  # noqa: E501
-        """Query Graph  # noqa: E501
+        """Query Graph (Deprecated)  # noqa: E501
 
-        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  # noqa: E501
+        Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  This has been deprecated in favor of the `/containers/{container_id}/data` endpoint.  # noqa: E501
         This method makes a synchronous HTTP request by default. To make an
         asynchronous HTTP request, please pass async_req=True
         >>> thread = api.query_graph_with_http_info(body, container_id, async_req=True)
--- docs/DataQueryApi.md
+++ docs/DataQueryApi.md
@@ -4,14 +4,15 @@ All URIs are relative to *http://localhost:8090*
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
-[**query_graph**](DataQueryApi.md#query_graph) | **POST** /containers/{container_id}/query | Query Graph
+[**query_data**](DataQueryApi.md#query_data) | **POST** /containers/{container_id}/data | Query Data
+[**query_graph**](DataQueryApi.md#query_graph) | **POST** /containers/{container_id}/query | Query Graph (Deprecated)
 
-# **query_graph**
-> InlineResponse200 query_graph(body, container_id)
+# **query_data**
+> InlineResponse200 query_data(body, container_id)
 
-Query Graph
+Query Data
 
-Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.
+Query data from your container using GraphQL. You can learn more here - https://gitlab.software.inl.gov/b650/Deep-Lynx/-/wikis/Querying-Data-With-GraphQL
 
 ### Example
 ```python
@@ -28,7 +29,58 @@ body = NULL # object |
 container_id = 'container_id_example' # str | 
 
 try:
-    # Query Graph
+    # Query Data
+    api_response = api_instance.query_data(body, container_id)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling DataQueryApi->query_data: %s\n" % e)
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **body** | [**object**](object.md)|  | 
+ **container_id** | **str**|  | 
+
+### Return type
+
+[**InlineResponse200**](InlineResponse200.md)
+
+### Authorization
+
+[BearerAuth](../README.md#BearerAuth)
+
+### HTTP request headers
+
+ - **Content-Type**: application/json, text/plain
+ - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
+# **query_graph**
+> InlineResponse200 query_graph(body, container_id)
+
+Query Graph (Deprecated)
+
+Query the graph of the specified container using GraphQL. GraphQL queries may be formatted as json or plain text.  This has been deprecated in favor of the `/containers/{container_id}/data` endpoint.
+
+### Example
+```python
+from __future__ import print_function
+import time
+import deep_lynx
+from deep_lynx.rest import ApiException
+from pprint import pprint
+
+
+# create an instance of the API class
+api_instance = deep_lynx.DataQueryApi(deep_lynx.ApiClient(configuration))
+body = NULL # object | 
+container_id = 'container_id_example' # str | 
+
+try:
+    # Query Graph (Deprecated)
     api_response = api_instance.query_graph(body, container_id)
     pprint(api_response)
 except ApiException as e:
